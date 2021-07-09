7e7ca90d3dae3b5e02a3e879f0fadda8474831ea
dpdahal <dp48dahal@gmail.com>
2021-07-09 19:45:45 +0545

    git test

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+# x = 10
+#
+# print(dir(x))
+# print(type(x))
+# name = 'ram'
+# print(dir(name))
+
+# Numeric
+
+
+# int, float, string, boolean,
+# tuple, set, list, dic, none
---
+++ introduction.py
@@ -0,0 +1,49 @@
+# print("Hello python")
+
+# myName = "Sophia"
+# print(myName)
+# '', or ""
+# myName = "Ram"
+# age = 20
+# print(myName + age)
+# print(f"Name: {myName}  Age: {age}")
+# print("My name is: ", myName)
+# print("Age: ", age)
+# print(myName, age)
+
+# print()
+#  string
+# name = input("Enter any name: ")
+#
+# print(f"You enter {name}")
+
+# x = int(input("Enter x: "))
+# y = int(input("Enter y: "))
+# print(x + y)
+
+# x = input("Enter x: ")
+# y = input("Enter y: ")
+# result = int(x) + int(y)
+# print(result)
+# print(x + y)
+
+# print("=======before======")
+# print(type(x))
+# print("=========After========")
+# x = int(x)
+# y = int(y)
+# print(x + y)
+# print(type(x))
+
+#  type cast
+
+# print(type(x))
+# string
+# print(x + y)
+
+# name = input("Enter name: ")
+# age = input("Enter age: ")
+# phone = input("Enter phone: ")
+# address = input("Enter address")
+#
+# print(f"Name: {name} Age: {age} Phone: {phone} Address: {address}")
